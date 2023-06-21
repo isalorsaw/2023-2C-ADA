9b26d50d613e119dfad888410e4efcb969f7bf4b
isalorsaw <isalorsaw@hotmail.com>
2023-06-20 20:19:40 -0600

    Inicio Java 20/6

---
+++ 2doParcial/Ejercicio1.java
@@ -0,0 +1,22 @@
+/*
+   Nombre: Ing Isaias Salinas
+   Numero Cuenta: 232051048
+   Clase Analisis y Diseno de Algoritmo
+   */
+//Librerias
+import javax.swing.JOptionPane;
+public class Ejercicio1
+{//Inicio de Class
+    public static void main(String args[])
+    {//Inicio Main
+        
+        //Declaraciones
+        int n1=0;
+        int n2=0;
+        int suma=0;
+        //Asignaciones o Inicializaciones
+        
+        String entrada=JOptionPane.showInputDialog("Ingrese el Primer Numero");
+        //Llegamos aqui por hoy
+    }//Fin del Main
+}//Fin de Class
